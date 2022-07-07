c76c14ebd5be76396292851b80c183924f66c3b4
Your Name~ <you@example.com>
2022-07-07 14:18:15 +0100

    React App version in motion

---
+++ profile-app/src/components/App/App.js
@@ -0,0 +1,27 @@
+import './app.css';
+import React from 'react';
+
+function App() {
+  return (
+    <body>
+  <div class="container">
+    <p class="msg">
+      Mabrouka Yaqoob
+    </p>
+  </div>
+
+  <div class="aboutme">
+  <p class="allaboutme">
+  About me
+    </p>
+
+  </div>
+  <footer>
+    Check out my <a href="https://github.com/BrookaY" target="_blank">Github</a>.
+  </footer>
+
+  </body>
+  );
+}
+
+export default App;
